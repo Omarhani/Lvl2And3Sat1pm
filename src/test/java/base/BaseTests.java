fce7f26f58bb8fd5e6d9a581629a0730153f65de
Omar Hany <OmarMohamed20150362@fci.helwan.edu.eg>
2023-09-09 15:00:31 +0300

    adding MethodHandles class, HomePage, LoginPage, BaseTests, and LoginTests Classes

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,32 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import pages.HomePage;
+import reader.ReadDataFromJson;
+
+import java.io.FileNotFoundException;
+
+public class BaseTests {
+
+    WebDriver driver;
+    protected ReadDataFromJson readDataFromJson;
+    protected HomePage homePage;
+
+    @BeforeClass
+    public void setUp() throws FileNotFoundException {
+        readDataFromJson = new ReadDataFromJson();
+        driver = new ChromeDriver();
+        driver.get(readDataFromJson.readJsonFile().URL);
+        driver.manage().window().maximize();
+        homePage = new HomePage(driver);
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+}
